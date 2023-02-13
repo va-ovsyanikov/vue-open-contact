2eda1a9fdc26b65d9532bf3fef4c66dc9242883a
va-ovsyanikov <va.ovsyanikov@gmail.com>
2023-02-13 15:18:37 +0300

    remove token

--- src/services/api.services.js
+++ src/services/api.services.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const endpoint = 'https://api.github.com/graphql'
-const token = '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'
+const token = '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'
 
 const headers = {
   'content-type': 'application/json',
